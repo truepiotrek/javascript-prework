c681d161935b8e69a3e20fc71fe2e9b362043a1c
Peter Slezak <piotrek.slezak@gmail.com>
2019-09-22 23:59:01 +0200

    update scrip.js with computerMove

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
-var computerMove, playerMove;
-computerMove = 'kamień';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-playerMove = 'papier';
-printMessage('Zagrałem ' + playerMove + '! Jeżeli Twóch ruch to papier, to mamy remis!');
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else if (randomNumber == '2') {
+  computerMove = 'papier';
+} else {
+  computerMove = 'nożyce';
+}
+printMessage('Mój ruch: ' + computerMove);
